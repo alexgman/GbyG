eb94a986c3f32a3a6729afb2515d0754e171c7a4
Alex Gordon <aleksandr.gordon@gmail.com>
2016-06-28 15:14:30 -0500

    UI will be committed as part of a separate solution.

--- UI/controllers/hello.js
+++
@@ -1,6 +0,0 @@
-﻿function Hello($scope, $http) {
-    $http.get('http://rest-service.guides.spring.io/greeting').
-        success(function (data) {
-            $scope.greeting = data;
-        });
-}
