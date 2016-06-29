7fb9d5f0753359270e0b94d10ba859b6d4c2ac78
Alex Gman <alexgman@users.noreply.github.com>
2016-06-28 19:41:54 -0500

    Update User.cs

--- GbyG/Models/User.cs
+++ GbyG/Models/User.cs
@@ -1,13 +1,9 @@
-﻿using Newtonsoft.Json;
-
 namespace GbyG.Models
 {
     public class User
     {
         public int UserId { get; set; }
-
         public string FirstName { get; set; }
-
         public string LastName { get; set; }
     }
 }
